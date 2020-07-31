e1240cf0216a7964468c77d2df0c783f512bb666
midroid <ch.m.aakash@gmail.com>
2020-07-31 14:17:47 +0900

    feature: tithi and paksh support

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ const MONTHS = [
 	"फाल्गुन:"
 ];
 
+const TITHIYA = [
+	"प्रतिपदा",
+	"द्वितीया",
+	"तृतीया",
+	"चतुर्थी",
+	"पंचमी",
+	"षष्ठी",
+	"सप्तमी",
+	"अष्टमी",
+	"नवमी",
+	"दशमी",
+	"एकादशी",
+	"द्वादशी",
+	"त्रयोदशी",
+	"चतुर्दशी",
+	"पूर्णिमा",
+	"अमावस्या"
+];
+
+const PAKSHO = [
+	"शुक्ल",
+	"कृष्ण"
+];
+
 exports.getMonths = function() {
 	return MONTHS;
+}
+
+exports.getMonths = function() {
+	return TITHIYA;
+}
+
+exports.getPaksho = function() {
+	return PAKSHO;
 }
